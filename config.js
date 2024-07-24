00d548ef491f55c063677d65c5666f3459d26429
thesanimoney <thesanimoney@gmail.com>
2024-07-24 09:25:51 +0300

    initial

---
+++ config.js
@@ -0,0 +1,2 @@
+require('dotenv').config();
+console.log(process.env.EMAIL_API_TOKEN);
---
+++ prisma/migrations/20240720054126_add_password/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `ResetPasswordToken` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `ResetPasswordToken_token_key`(`token`),
+    UNIQUE INDEX `ResetPasswordToken_email_token_key`(`email`, `token`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
